e5672587d613bdd91e724b59d1143e598f61d55d
JulianRamundo <julianramundo95@gmail.com>
2022-02-04 13:23:45 -0300

    Ejercicios Intro Completados

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -2,16 +2,75 @@
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+************************************************************************
+Alumno: Julian Ramundo DIV Z 
+Tp nro1 
+
 */
+
 function Sumar () 
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultado;
+
+			precio1= document.getElementById("txtIdPrecioUno").value;
+			precio1= parseInt(precio1);
+				precio2= document.getElementById("txtIdPrecioDos").value;
+				precio2= parseInt(precio2);
+					precio3= document.getElementById("txtIdPrecioTres").value;
+					precio3= parseInt(precio3);
+
+				resultado = precio1 + precio2 + precio3;
+
+alert("la suma es: "+ resultado );	
 	
 }
 function Promedio () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultado;
+
+			precio1= document.getElementById("txtIdPrecioUno").value;
+			precio1= parseInt(precio1);
+				precio2= document.getElementById("txtIdPrecioDos").value;
+				precio2= parseInt(precio2);
+					precio3= document.getElementById("txtIdPrecioTres").value;
+					precio3= parseInt(precio3);
+
+
+				resultado= precio1+precio2+precio3;
+
+alert("el promedio es : "+ resultado/3);
+
+
 }
+
 function PrecioFinal () 
 {
 	
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultado;
+
+			precio1= document.getElementById("txtIdPrecioUno").value;
+			precio1= parseInt(precio1);
+				precio2= document.getElementById("txtIdPrecioDos").value;
+				precio2= parseInt(precio2);
+					precio3= document.getElementById("txtIdPrecioTres").value;
+					precio3= parseInt(precio3);
+
+
+				resultado= precio1+precio2+precio3;
+
+alert("el precio final es : "+ resultado * 1.21);
+
+
+
+
+
 }
